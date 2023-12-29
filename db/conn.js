93aeac6b6abd49b2e11895e11607de03bc491eeb
Luiz-Coelho <luizfelipecs99@gmail.com>
2023-12-29 06:56:18 -0300

    update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 
+const dbUri = process.env.DB_URI;
+
+const uri = `${dbUri}`;
+
 async function main() {
   try {
-    await mongoose.connect(process.env.DB_URI);
+    await mongoose.connect(uri);
     console.log("Conectado ao banco de dados!");
   } catch (error) {
     console.log(`Error: ${error}`);
