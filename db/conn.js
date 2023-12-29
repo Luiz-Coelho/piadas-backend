a2cb6c0b2c13b575828c10957a5e17f2b144e5ff
Luiz-Coelho <luizfelipecs99@gmail.com>
2023-12-29 06:46:33 -0300

    update URI db connection

--- db/conn.js
+++ db/conn.js
@@ -1,14 +1,8 @@
 const mongoose = require("mongoose");
 
-const dbUser = process.env.DB_USERNAME;
-const dbPassword = process.env.DB_PASSWORD;
-const dbUrl = process.env.DB_URL;
-
-const url = `mongodb+srv://${dbUser}:${dbPassword}${dbUrl}`;
-
 async function main() {
   try {
-    await mongoose.connect(url);
+    await mongoose.connect(process.env.DB_URI);
     console.log("Conectado ao banco de dados!");
   } catch (error) {
     console.log(`Error: ${error}`);
